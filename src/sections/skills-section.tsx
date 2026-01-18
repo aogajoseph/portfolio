6911ab693e792f7efed21aa3489799bfa64f324f
aogajoseph <aogathesecond@gmail.com>
2026-01-18 12:51:23 +0300

    Update CV and Skills

--- src/sections/skills-section.tsx
+++ src/sections/skills-section.tsx
@@ -11,7 +11,8 @@ export default function SkillsSection() {
         "Git",
         "API Integration",
         "System Design",
-        "Debugging"
+        "Debugging",
+        "Collaboration"
     ];
 
     return (
